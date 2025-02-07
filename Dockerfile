6851af2e7383a5846dd02a2c6a63d270ca306d26
Saif Alaqqad <saifo1999@gmail.com>
2025-02-07 17:05:20 +0300

    Add Scalar + APIKey authentication + Cleanup code

--- Dockerfile
+++ Dockerfile
@@ -3,11 +3,15 @@ FROM python:3.13
 WORKDIR /code
 
 COPY ./requirements.txt /code/requirements.txt
-COPY ./torch-requirements.txt /code/torch-requirements.txt
+COPY ./torch-gpu-requirements.txt /code/torch-gpu-requirements.txt
+# COPY ./torch-cpu-requirements.txt /code/torch-cpu-requirements.txt
 
-RUN pip install --no-cache-dir --upgrade -r /code/torch-requirements.txt
+# RUN pip install --no-cache-dir --upgrade -r /code/torch-cpu-requirements.txt
+RUN pip install --no-cache-dir --upgrade -r /code/torch-gpu-requirements.txt
+
 RUN pip install --no-cache-dir --upgrade -r /code/requirements.txt
 
 COPY ./app /code/app
 
+ENV USE_GPU true
 CMD ["fastapi", "run", "app/main.py", "--port", "8000"]
--- app/main.py
+++ app/main.py
@@ -1,31 +1,50 @@
-from transformers import AutoModelForCausalLM
-from fastapi import FastAPI, File, UploadFile, Form
-from PIL import Image
-from typing import Annotated
 import io
 import os
+from typing import Annotated
 
-# vipsbin = r'C:\Users\saifw\OneDrive\Desktop\moondream\vips-dev-8.16\bin'
+import scalar_fastapi.scalar_fastapi as scalar
+from fastapi import Depends, FastAPI, File, Form, HTTPException, UploadFile
+from fastapi.security import APIKeyHeader
+from PIL import Image
+from starlette.responses import RedirectResponse
+from transformers import AutoModelForCausalLM
+
+# On windows you'll need to set the path to vips
+# vipsbin = r'<vips/bin/path>'
 # add_dll_dir = getattr(os, 'add_dll_directory', None)
 # if callable(add_dll_dir):
 #     add_dll_dir(vipsbin)
 # else:
 #     os.environ['PATH'] = os.pathsep.join((vipsbin, os.environ['PATH']))
 
+default_api_key = os.getenv("DEFAULT_API_KEY", "1234")
+
+using_gpu = os.getenv("USE_GPU", "") == "true"
+if using_gpu:
+    print("Loading GPU model")
+else:
+    print("Loading CPU model")
+
 model = AutoModelForCausalLM.from_pretrained(
     "vikhyatk/moondream2",
     revision="2025-01-09",
     trust_remote_code=True,
-    # Uncomment to run on GPU.
-    # device_map="cuda",
+    device_map=("cuda" if using_gpu else "cpu"),
 )
 
-app = FastAPI()
+app = FastAPI(title="Moondream API")
+header_scheme = APIKeyHeader(name="X-Api-Key", scheme_name="API Key Header")
 
 
-# "Give me a json array output of the items left on the backseats of the car"
 @app.post("/query/")
-async def query_moondream(file: UploadFile, prompt: Annotated[str, Form()]):
+async def query_moondream(
+    file: UploadFile,
+    prompt: Annotated[str, Form()],
+    api_key: str = Depends(header_scheme),
+):
+    if api_key != default_api_key:
+        raise HTTPException(status_code=401, detail="Invalid Credentials")
+
     # Read the contents of the uploaded file into a BytesIO object
     contents = await file.read()  # async read
     image = Image.open(io.BytesIO(contents))
@@ -40,3 +59,15 @@ async def query_moondream(file: UploadFile, prompt: Annotated[str, Form()]):
         return {"result": result}
     except (IOError, SyntaxError):
         return {"message": "Invalid image file"}
+
+
+@app.get("/", include_in_schema=False)
+def default_redirect():
+    return RedirectResponse(url="/scalar")
+
+
+@app.get("/scalar", include_in_schema=False)
+async def scalar_html():
+    return scalar.get_scalar_api_reference(
+        openapi_url=app.openapi_url, title=app.title, layout=scalar.Layout.CLASSIC
+    )
